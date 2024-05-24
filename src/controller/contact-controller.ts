7b08aea6e3545b748bb5c91b2fd92bb5a0465447
Ravi <samcollins9899@gmail.com>
2024-05-24 18:01:28 +0530

    feat: updated controller logic with secondary contacts

--- src/controller/contact-controller.ts
+++ src/controller/contact-controller.ts
@@ -32,6 +32,46 @@ async function identify(req: express.Request, res: express.Response) {
         },
       });
     }
+
+    let primaryContact = contacts.find((contact) => contact.linkPrecedence === "primary");
+    if (!primaryContact) {
+      primaryContact = contacts[0];
+    }
+
+    // Update secondary contacts to link to  pri. contact
+    for (const contact of contacts) {
+      if (contact.id !== primaryContact.id) {
+        await prisma.contact.update({
+          where: { id: contact.id },
+          data: {
+            linkedId: primaryContact.id,
+            linkPrecedence: "secondary",
+          },
+        });
+      }
+    }
+
+    const emails = new Set<string>();
+    const phoneNumbers = new Set<string>();
+    const secondaryContactIds = [];
+
+    for (const contact of contacts) {
+      if (contact.email) emails.add(contact.email);
+      if (contact.phoneNumber) phoneNumbers.add(contact.phoneNumber);
+      if (contact.id !== primaryContact.id) secondaryContactIds.push(contact.id);
+    }
+
+    if (email) emails.add(email);
+    if (phoneNumber) phoneNumbers.add(phoneNumber);
+
+    res.json({
+      contact: {
+        primaryContactId: primaryContact.id,
+        emails: Array.from(emails),
+        phoneNumbers: Array.from(phoneNumbers),
+        secondaryContactIds,
+      },
+    });
   } catch (error) {
     console.log(error);
   }
